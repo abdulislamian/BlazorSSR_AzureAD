c9890e4bbd47fa505a9bbab5ea0a25bda37fc244
Abdullah Khan <abdulislamian@outlook.com>
2024-01-29 18:09:41 +0500

    Add project files.

---
+++ BlazorSsrWithAzureAD/Program.cs
@@ -0,0 +1,46 @@
+using BlazorSsrWithAzureAD.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(options =>
+    {
+        builder.Configuration.Bind(Constants.AzureAd, options);
+        // TODO - remove this line when token validation issue is fixed.
+        options.TokenValidationParameters.ValidateIssuer = false;
+    });
+builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+builder.Services.AddCascadingAuthenticationState();
+
+//builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapControllers();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
